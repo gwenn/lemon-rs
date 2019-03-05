6923a6d6aae2689bbe61ede89066ba7dc16c07dd
gwenn <gtreguier@gmail.com>
2019-03-05 20:50:33 +0100

    Add an example to parse a single command

---
+++ lexer/examples/sql_cmd.rs
@@ -0,0 +1,24 @@
+use env_logger;
+
+use fallible_iterator::FallibleIterator;
+use sqlite_lexer::sql::Parser;
+
+/// Parse a string.
+// RUST_LOG=sqlite3Parser=debug
+fn main() {
+    env_logger::init();
+    let arg = "PRAGMA parser_trace=ON;";
+    let mut parser = Parser::new(arg.as_bytes());
+    loop {
+        match parser.next() {
+            Ok(None) => break,
+            Err(err) => {
+                eprintln!("Err: {} in {}", err, arg);
+                break;
+            }
+            Ok(Some(cmd)) => {
+                println!("{}", cmd);
+            }
+        }
+    }
+}
