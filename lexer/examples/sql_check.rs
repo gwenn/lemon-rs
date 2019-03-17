125d3ae4b90a771320062d1eef5b808048d140b4
gwenn <gtreguier@gmail.com>
2019-03-17 09:33:52 +0100

    Check generated ASTs

---
+++ lexer/examples/sql_check.rs
@@ -0,0 +1,56 @@
+use env_logger;
+use fallible_iterator::FallibleIterator;
+use std::env;
+use std::fs::File;
+use std::panic;
+
+use sqlite_lexer::sql::Parser;
+
+/// Parse specified files and check all commands.
+fn main() {
+    env_logger::init();
+    let args = env::args();
+    for arg in args.skip(1) {
+        println!("{}", arg);
+        let result = panic::catch_unwind(|| {
+            let f = File::open(arg.clone()).unwrap();
+            let mut parser = Parser::new(f);
+            loop {
+                match parser.next() {
+                    Ok(None) => break,
+                    Err(err) => {
+                        eprintln!("Err: {} in {}", err, arg);
+                        break;
+                    }
+                    Ok(Some(cmd)) => {
+                        let input = cmd.to_string();
+                        let mut checker = Parser::new(input.as_bytes());
+                        match checker.next() {
+                            Err(err) => {
+                                eprintln!(
+                                    "Check Err in {}:{}, {} in\n{}\n{:?}",
+                                    arg,
+                                    parser.line(),
+                                    err,
+                                    input,
+                                    cmd
+                                );
+                            }
+                            Ok(None) => {
+                                eprintln!("Check Err in {}:{}, {:?}", arg, parser.line(), cmd);
+                            }
+                            Ok(Some(check)) => {
+                                if cmd != check {
+                                    eprintln!("{:?}\n<>\n{:?}", cmd, check);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        });
+        if let Err(e) = result {
+            eprintln!("Panic: {:?} in {}", e, arg);
+        }
+    }
+}
