5197dd209c59876cbc2948c09040a20a2ca85246
gwenn <gtreguier@gmail.com>
2021-12-18 08:17:34 +0100

    Fix clippy warning

--- src/lexer/sql/mod.rs
+++ src/lexer/sql/mod.rs
@@ -237,14 +237,12 @@ impl Tokenizer {
 /// use sqlite3_parser::lexer::sql::Tokenizer;
 /// use sqlite3_parser::lexer::Scanner;
 ///
-/// fn main() {
-///     let tokenizer = Tokenizer::new();
-///     let input = "PRAGMA parser_trace=ON;".as_bytes();
-///     let mut s = Scanner::new(input, tokenizer);
-///     let (token1, _) = s.scan().unwrap().unwrap();
-///     s.scan().unwrap().unwrap();
-///     assert!(b"PRAGMA".eq_ignore_ascii_case(token1));
-/// }
+/// let tokenizer = Tokenizer::new();
+/// let input = "PRAGMA parser_trace=ON;".as_bytes();
+/// let mut s = Scanner::new(input, tokenizer);
+/// let (token1, _) = s.scan().unwrap().unwrap();
+/// s.scan().unwrap().unwrap();
+/// assert!(b"PRAGMA".eq_ignore_ascii_case(token1));
 /// ```
 impl Splitter for Tokenizer {
     type Error = Error;
