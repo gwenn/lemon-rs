230dd8b849e921f7e46feb373d5ae013e7f0848d
gwenn <gtreguier@gmail.com>
2024-03-16 07:44:39 +0100

    Make sql_cmd example parse args

--- examples/sql_cmd.rs
+++ examples/sql_cmd.rs
@@ -1,21 +1,25 @@
+use std::env;
+
 use fallible_iterator::FallibleIterator;
 use sqlite3_parser::lexer::sql::Parser;
 
-/// Parse a string.
+/// Parse args.
 // RUST_LOG=sqlite3Parser=debug
 fn main() {
     env_logger::init();
-    let arg = "PRAGMA parser_trace=ON;";
-    let mut parser = Parser::new(arg.as_bytes());
-    loop {
-        match parser.next() {
-            Ok(None) => break,
-            Err(err) => {
-                eprintln!("Err: {err} in {arg}");
-                break;
-            }
-            Ok(Some(cmd)) => {
-                println!("{cmd}");
+    let args = env::args();
+    for arg in args.skip(1) {
+        let mut parser = Parser::new(arg.as_bytes());
+        loop {
+            match parser.next() {
+                Ok(None) => break,
+                Err(err) => {
+                    eprintln!("Err: {err} in {arg}");
+                    break;
+                }
+                Ok(Some(cmd)) => {
+                    println!("{cmd}");
+                }
             }
         }
     }
