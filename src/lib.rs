4a37b20536b3d6c25d8a498365a4fb424786d092
gwenn <45554+gwenn@users.noreply.github.com>
2026-06-14 09:20:49 +0200

    Make clippy warn on large stack frames

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 //! SQLite3 syntax lexer and parser
 #![warn(missing_docs)]
-//#![warn(clippy::large_stack_frames)] doesn't take into account opt-level / --release flag
 
 pub use bumpalo::{collections::Vec, Bump};
 pub use fallible_iterator::FallibleIterator;
